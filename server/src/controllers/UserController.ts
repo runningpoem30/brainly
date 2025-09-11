c840f375abc57173325cbb96ef4f3cdbb6573872
runningpoem30 <aryaanandpathak30@gmail.com>
2025-09-11 23:41:46 +0530

    changes

--- server/src/controllers/UserController.ts
+++ server/src/controllers/UserController.ts
@@ -1,11 +1,15 @@
 import { UserModel } from "../models/UserModel";
-
+import bcrypt from "bcrypt"
 
 async function signUp(req : any  ,res : any ){
 
+
+    //doing the zod validation , hashing the password
     const {email , username , password } = req.body
+
+    const hashedPassword = bcrypt.hash(password , 10)
     
    const newUser  = await UserModel.create({
-      email :  email  , username : username , password : password
+      email :  email  , username : username , password : hashedPassword
    })
 }
