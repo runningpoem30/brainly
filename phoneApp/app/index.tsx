f6f2d19914e3b83fd9e1142b28b9a7586477e812
vinaykumar-hash <vinaykumar7525@gmail.com>
2025-09-14 13:01:02 +0530

    added react native blank project

---
+++ phoneApp/app/index.tsx
@@ -0,0 +1,15 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text className="text-2xl font-bold ">WELCOME!!</Text>
+    </View>
+  );
+}
