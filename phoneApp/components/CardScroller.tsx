222466deeacc8882a42e61de81ee110a71183cbb
vinaykumar-hash <vinaykumar7525@gmail.com>
2025-09-18 03:01:27 +0530

    adding content to mySpace page

--- phoneApp/components/CardScroller.tsx
+++ phoneApp/components/CardScroller.tsx
@@ -1,17 +1,24 @@
 // components/CardScroller.tsx
 import React from "react";
 import { ScrollView, View, StyleSheet } from "react-native";
+import {WebView} from "react-native-webview";
 
 export default function CardScroller() {
     return (
         <View>
         <ScrollView
             horizontal
-            showsHorizontalScrollIndicator={false}
+            showsHorizontalScrollIndicator={true}
             contentContainerStyle={styles.container}
         >
-            <View style={styles.card} />
-            <View style={styles.card} />
+            <View style={styles.card}>
+                <WebView source={{ uri: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRVJPWCnWpPYDyqvpV0loSZuiI1bCPaQMN-mg&s" }}
+                         />
+            </View>
+            <View style={styles.card}>
+                <WebView source={{ uri: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTpgHSO7GzRDFQ4nwbppkWvmYS5qvFlkOknKQ&s" }}
+                />
+            </View>
             <View style={styles.card} />
             <View style={styles.card} />
         </ScrollView>
@@ -24,13 +31,15 @@ const styles = StyleSheet.create({
     container: {
         gap: 16,
         paddingVertical: 10,
-        width: "100%",
+        // width: "100%",
+        paddingHorizontal: 16
     },
     card: {
         width: 120,
         height: 180,
         backgroundColor: "#d1d1d1",
         borderRadius: 30,
+        overflow: "hidden",
     },
     divider: {
         height: 0.5,
