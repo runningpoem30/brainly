8796c3e78f9025d0c49a78958546f50bf1e2f218
runningpoem30 <aryaanandpathak30@gmail.com>
2025-09-21 14:28:52 +0530

    G

--- server/src/models/ShareModel.ts
+++ server/src/models/ShareModel.ts
@@ -3,7 +3,10 @@ import mongoose from "mongoose";
 const ShareSchema = new mongoose.Schema({
         hash : String,
         userId : {type : mongoose.Types.ObjectId , ref : 'User' , required : true},
-        contentId : {type : mongoose.Types.ObjectId , ref : 'Content' , required : true }
+        contentId : {type : mongoose.Types.ObjectId , ref : 'ContentModel' , required : true }
 })
 
 const ShareModel = mongoose.model("ShareModel"  , ShareSchema)
+
+
+
